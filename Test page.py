41883fb8c03ad756c23179ce64fb6f424627d1fe
Lord-Of-The-Pies <110463168+Lord-Of-The-Pies@users.noreply.github.com>
2022-08-02 13:25:12 +0100

    Learning how to integrate pycharm and github using first Udemy course exam.
    New page introduced: Completed Game.
    This now holds the full completed code for a 'never ending' game of tic tac toe.
    
    Code allows for:
    accepting player name inputs at the start of the first game, but not afterwards.
    running score is maintained
    players can choose who goes first each round
    upon quiting game it will display a final total score

--- Test page.py
+++ Test page.py
@@ -1,6 +1,8 @@
 row1 = ['1',  '2',  '3']
 row2 = ['4',  '5',  '6']
 row3 = ['7',  '8',  '9']
+player_0 = "bob"
+player_x = "jill"
 player_x_counter = 4
 player_0_counter = 4
 player_x_score = 0
@@ -10,8 +12,6 @@ player_x_counter_display = player_x_counter * "X"
 taken_numbers = []
 win = False
 game_count = 0
-player_0 = "bob"
-player_x = "jill"
 
 
 def game_board():
@@ -19,9 +19,8 @@ def game_board():
     print(f"              {row1}                 ")
     print(f"     {player_0_score}        {row2}        {player_x_score}")
     print(f"              {row3}")
-    print(f"   {player_0_counter_display}                           {player_x_counter_display}")
+    print(f"   {player_0_counter_display}                            {player_x_counter_display}")
 
 
 print(game_board())
 
-print(f"TEST TEST FROM GITHUB EDIT TEST TEST")
