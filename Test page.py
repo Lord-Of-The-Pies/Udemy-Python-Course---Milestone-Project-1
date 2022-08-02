501e30fecfa2187fbfbf5c2b8bd6f0443873e6ec
Lord-Of-The-Pies <110463168+Lord-Of-The-Pies@users.noreply.github.com>
2022-08-02 12:21:22 +0100

    Learning how to integrate pycharm and github using first Udemy course exam.

---
+++ Test page.py
@@ -0,0 +1,25 @@
+row1 = ['1',  '2',  '3']
+row2 = ['4',  '5',  '6']
+row3 = ['7',  '8',  '9']
+player_x_counter = 4
+player_0_counter = 4
+player_x_score = 0
+player_0_score = 0
+player_0_counter_display = player_0_counter * "0"
+player_x_counter_display = player_x_counter * "X"
+taken_numbers = []
+win = False
+game_count = 0
+player_0 = "bob"
+player_x = "jill"
+
+
+def game_board():
+    print(f"    {player_0}                             {player_x}")
+    print(f"              {row1}                 ")
+    print(f"     {player_0_score}        {row2}        {player_x_score}")
+    print(f"              {row3}")
+    print(f"   {player_0_counter_display}                           {player_x_counter_display}")
+
+
+print(game_board())
